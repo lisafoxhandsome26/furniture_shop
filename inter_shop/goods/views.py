18144b99d7acdbed78d16b464677b4817a92beca
Пользователь <sashasergeevasus345@gmail.com>
2024-09-13 20:00:09 +0300

    added app goods

---
+++ inter_shop/goods/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Products
+
+
+def catalog(request):
+    goods = Products.objects.all()
+    context = {
+        "title": "Home - Каталог",
+        "goods": goods,
+    }
+    return render(request, "goods/catalog.html", context)
+
+
+def product(request):
+    return render(request, "goods/product.html")
