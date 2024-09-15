84fa9fbef809a4dcd4bdd4b9a79b99319ffb05cd
Пользователь <sashasergeevasus345@gmail.com>
2024-09-15 21:17:06 +0300

    Second pagination option GET params

--- inter_shop/goods/urls.py
+++ inter_shop/goods/urls.py
@@ -5,6 +5,5 @@ app_name = "goods"
 
 urlpatterns = [
     path('<slug:categories_slug>/', v.catalog, name='index'),
-    path('<slug:categories_slug>/<int:page>/', v.catalog, name='index'),
     path('product/<slug:product_slug>/', v.product, name='product'),
 ]
--- inter_shop/goods/views.py
+++ inter_shop/goods/views.py
@@ -4,13 +4,14 @@ from .models import Products
 from django.http import Http404
 
 
-def catalog(request, categories_slug, page=1):
+def catalog(request, categories_slug):
+    page = request.GET.get("page", 1)
     if categories_slug == "All":
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=categories_slug))
     paginator = Paginator(goods, 3)
-    curent_page = paginator.page(page)
+    curent_page = paginator.page(int(page))
     context = {
         "title": "Home - Каталог",
         "goods": curent_page,
