0dd6a4d593868bb0c4daf3a409f99ca471006862
Пользователь <sashasergeevasus345@gmail.com>
2024-09-16 20:26:19 +0300

    simple search created

--- inter_shop/goods/urls.py
+++ inter_shop/goods/urls.py
@@ -4,6 +4,7 @@ from goods import views as v
 app_name = "goods"
 
 urlpatterns = [
+    path('search/', v.catalog, name='search'),
     path('<slug:categories_slug>/', v.catalog, name='index'),
     path('product/<slug:product_slug>/', v.product, name='product'),
 ]
