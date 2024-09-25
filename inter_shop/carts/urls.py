f41b0bc7f9be3cb1cc475aa58f450de90f5bdeea
Пользователь <sashasergeevasus345@gmail.com>
2024-09-25 21:47:10 +0300

    Remove from cart ajax and json response

--- inter_shop/carts/urls.py
+++ inter_shop/carts/urls.py
@@ -4,7 +4,7 @@ from carts import views as v
 app_name = "carts"
 
 urlpatterns = [
-    path('cart_add/<slug:product_slug>/', v.cart_add, name='cart_add'),
-    path('cart_change/<slug:product_slug>/', v.cart_change, name='cart_change'),
-    path('cart_remove/<int:cart_id>/', v.cart_remove, name='cart_remove'),
+    path('cart_add/', v.cart_add, name='cart_add'),
+    path('cart_change/', v.cart_change, name='cart_change'),
+    path('cart_remove/', v.cart_remove, name='cart_remove'),
 ]
---
+++ inter_shop/carts/utils.py
@@ -0,0 +1,6 @@
+from carts.models import Cart
+
+
+def get_user_cart(request):
+    if request.user.is_authenticated:
+        return Cart.objects.filter(user=request.user.id)
