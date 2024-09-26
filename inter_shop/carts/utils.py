731baee8b4a1e7c7cba9d91bcbc1d751c70b97c7
Пользователь <sashasergeevasus345@gmail.com>
2024-09-26 15:16:43 +0300

    Session cart for anonymous user

--- inter_shop/carts/utils.py
+++ inter_shop/carts/utils.py
@@ -4,3 +4,7 @@ from carts.models import Cart
 def get_user_cart(request):
     if request.user.is_authenticated:
         return Cart.objects.filter(user=request.user.id)
+
+    if not request.session.session_key:
+        request.session.create()
+    return Cart.objects.filter(session_key=request.session.session_key)
--- inter_shop/users/views.py
+++ inter_shop/users/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
+from carts.models import Cart
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
 
@@ -16,9 +17,12 @@ def login(request):
             username = request.POST['username']
             password = request.POST['password']
             user = auth.authenticate(username=username, password=password)
+            session_key = request.session.session_key
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username} вы вошли в аккаунт")
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
                 redirect_page = request.POST.get("next", None)
                 if redirect_page and redirect_page != reverse('user:logout'):
                     return HttpResponseRedirect(request.POST.get("next"))
@@ -38,8 +42,11 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            session_key = request.session.session_key
             user = form.instance
             auth.login(request, user)
+            if session_key:
+                Cart.objects.filter(session_key=session_key).update(user=user)
             messages.success(request, f"{user.username} вы успешно зарегестрировались и вошли в аккаунт")
             return HttpResponseRedirect(reverse('main:index'))
     else:
