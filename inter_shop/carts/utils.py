2a56b77a85ee8a01e703ca755531893e58277d13
Пользователь <sashasergeevasus345@gmail.com>
2024-09-28 18:20:46 +0300

    Final changes

--- inter_shop/carts/utils.py
+++ inter_shop/carts/utils.py
@@ -3,8 +3,8 @@ from carts.models import Cart
 
 def get_user_cart(request):
     if request.user.is_authenticated:
-        return Cart.objects.filter(user=request.user.id)
+        return Cart.objects.filter(user=request.user.id).select_related('product')
 
     if not request.session.session_key:
         request.session.create()
-    return Cart.objects.filter(session_key=request.session.session_key)
+    return Cart.objects.filter(session_key=request.session.session_key).select_related('product')
